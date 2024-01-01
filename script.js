482805a40d56e9935917740242c328cc67e6d09b
Michaella Rodriguez <micha.ella@hotmail.com>
2024-01-01 17:28:11 -0600

    Testing out calculations with input values.

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const lapEl = document.getElementById("lap-num")
+const formEl = document.getElementById("form-el")
+const totalEl = document.getElementById("total-el")
 
-let laps = 20
+formEl.addEventListener("submit", calculateDailyMiles)
+
+let laps 
 let yards = 66
 
-const totalYards = laps * yards
 const yardsPerMile = 1760
-const dailyMiles = totalYards / yardsPerMile
+let totalYards = laps * yards
+let dailyMiles = totalYards / yardsPerMile
 
 
 let totalMiles = 0
 totalMiles += dailyMiles
 
-document.body.innerHTML += `<p>${totalMiles}</p>`
+// document.body.innerHTML += `<p>${totalMiles}</p>`
+
+function calculateDailyMiles(e) {
+    e.preventDefault()
+    laps = lapEl.value 
+    let totalYards = laps * yards
+    let dailyMiles = totalYards / yardsPerMile
+    totalEl.textContent += dailyMiles
+}
