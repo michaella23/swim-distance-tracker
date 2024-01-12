d4d5c3142b77c2a592dac9b845bdfd6c4a13d950
Michaella Rodriguez <micha.ella@hotmail.com>
2024-01-12 14:56:07 -0600

    Created lapsData array, rendered data dynamically (still hard coded)

---
+++ laps.js
@@ -0,0 +1,22 @@
+export const lapsData = [
+    {
+        date: "01/03",
+        laps: 26.5,
+        miles: 1.0
+    },
+    {
+        date: "01/07",
+        laps: 23,
+        miles: 0.9
+    },
+    {
+        date: "01/11",
+        laps: 26.5,
+        miles: 1.0
+    },
+    {
+        date: "01/12",
+        laps: 26.5,
+        miles: 1.0
+    }
+]
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { lapsData } from "./laps.js"
+
 const lapEl = document.getElementById("lap-num")
 const formEl = document.getElementById("form-el")
 const totalEl = document.getElementById("total-el")
@@ -24,3 +26,35 @@ function calculateDailyMiles(e) {
     let dailyMiles = (totalYards / yardsPerMile).toFixed(1)
     totalEl.textContent += dailyMiles
 }
+
+const statsEl = document.querySelector(".stats-el")
+console.log(statsEl)
+
+function renderData() {
+    const swimData = lapsData.map((entry) => {
+        statsEl.innerHTML += `
+            <div class="daily-stat">
+                    <p>${entry.date}</p>
+                    <p>${entry.laps}</p>
+                    <p>${entry.miles}</p>
+            </div>`
+        // const dailyEntry = document.createElement("div")
+        // dailyEntry.setAttribute("class", "daily-stat")
+        // const dateEl = document.createElement("p")
+        // dateEl.textContent = entry.date
+        // // console.log(date.textContent)
+        // const lapsEl = document.createElement("p")
+        // lapsEl.textContent = entry.laps
+        // const milesEl = document.createElement("p")
+        // milesEl.textContent = entry.miles
+        // console.log(milesEl)
+        // dailyEntry.innerHTML += dateEl
+        // console.log(dailyEntry)
+        // dailyEntry.appendChild = lapsEl
+        // dailyEntry.appendChild = milesEl
+    })
+    return swimData
+}
+
+renderData()
+
