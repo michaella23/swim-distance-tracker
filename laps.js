cfee61c899b355f02f02c71f07cb58ade222c764
Michaella Rodriguez <micha.ella@hotmail.com>
2024-01-18 21:21:15 -0600

    entries for 1/17, 1/18, pool names

--- laps.js
+++ laps.js
@@ -2,26 +2,43 @@ export const lapsData = [
     {
         date: "01/03",
         laps: 26.5,
-        miles: 1.0
+        miles: 1.0,
+        pool: "Big Stacy"
     },
     {
         date: "01/07",
         laps: 23,
-        miles: 0.9
+        miles: 0.9,
+        pool: "Deep Eddy"
     },
     {
         date: "01/11",
         laps: 26.5,
-        miles: 1.0
+        miles: 1.0,
+        pool: "Big Stacy"
     },
     {
         date: "01/12",
         laps: 26.5,
-        miles: 1.0
+        miles: 1.0,
+        pool: "Big Stacy"
     },
     {
         date: "01/13",
         laps: 30.5,
-        miles: 0.9
+        miles: 0.9,
+        pool: "Bartholomew"
+    },
+    {
+        date: "01/17",
+        laps: 23.5,
+        miles: 0.9,
+        pool: "Big Stacy"
+    },
+    {
+        date: "01/18",
+        laps: 26.5,
+        miles: 1.0,
+        pool: "Big Stacy"
     }
 ]
