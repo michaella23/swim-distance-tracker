68434d3a2f59352b188e0eeda14acdcd3f22eefc
Michaella Rodriguez <micha.ella@hotmail.com>
2024-02-15 18:55:13 -0600

    config fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, getDoc } from 'firebase/firestore/lite';
 
+const firebaseApp = initializeApp({ /* config */ });
+const db = getFirestore(firebaseApp);
+
+async function loadCity(name) {
+  const cityDoc = doc(db, `cities/${name}`);
+  const snapshot = await getDoc(cityDoc);
+  return {
+    id: snapshot.id,
+    ...snapshot.data(),
+  };
+}
 
 import { lapsData } from "./laps.js"
 
